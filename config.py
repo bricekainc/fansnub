f9c758d90664c80c8fd246e2abf8aab172d6b1cf
KVO <121897808+kariuki727@users.noreply.github.com>
2025-06-22 02:40:33 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+FEED_URLS = os.getenv("FEED_URLS", "").split(",")
